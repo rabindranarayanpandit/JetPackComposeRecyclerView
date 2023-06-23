aebaedabe9b363ef71805f16d4411e432385cb99
rabindra <Rabindra.Narayan@sephora.com>
2023-06-23 14:07:24 +0530

    JetPack Product List Display

--- app/src/main/java/com/techskill/jetpackcomposerecyclerview/MainActivity.kt
+++ app/src/main/java/com/techskill/jetpackcomposerecyclerview/MainActivity.kt
@@ -3,13 +3,20 @@ package com.techskill.jetpackcomposerecyclerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.techskill.jetpackcomposerecyclerview.screens.DetailsContent
 import com.techskill.jetpackcomposerecyclerview.ui.theme.JetPackComposeRecyclerViewTheme
 
@@ -19,9 +26,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetPackComposeRecyclerViewTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Magenta)
                 ) {
                     DetailsContent()
                 }
